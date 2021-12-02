8201279593134a2d050a6e8bb881ae7c110d0d52
Ella Ahlstedt <ella.ahlstedt@learnet.se>
2021-12-02 14:48:18 +0100

    updated css

--- module5_laravel/resources/views/pages/index.blade.php
+++ module5_laravel/resources/views/pages/index.blade.php
@@ -4,9 +4,24 @@
 
 <section class="index">
 
-    <div class="container">
+    <div class="container text-center" style="width: 100%; ">
 
-        <h1>Genealogy</h1>
+        <div class="title">
+            <h1 style="font-size: 100px;">Genealogy</h1>
+        </div>
+        <div class="subheading">
+            <p>...create or load World lorem ipsum dolor imet sit lorem ipsum World lorem ipsum dolor imet sit lorem ipsum</p>
+        </div>
+        <div class="buttons row">
+            <div class="col-lg-6">
+                <a href="" class="btn btn-lg btn-gradient">Create</a>
+            </div>
+            <div class="col-lg-6">
+                <a href="" class="btn btn-lg btn-outline-gradient"><span>Load</span></a>
+            </div>
+        </div>
+        
+
 
     </div>
 
