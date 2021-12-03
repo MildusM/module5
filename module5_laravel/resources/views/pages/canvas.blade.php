ba754606f989a8526f550fae57a651d046dcda06
Ella Ahlstedt <ella.ahlstedt@learnet.se>
2021-12-03 11:49:36 +0100

    canvas done

---
+++ module5_laravel/resources/views/pages/canvas.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts/app')
+
+@section('content')
+
+    <canvas id="canvas" class="viewCanvas"></canvas>
+        
+        <div class="gradient canvas-btn">
+            <a href="/edit"><button class="btn btn-light">Edit</button></a>
+            <button class="btn btn-outline-light">Delete</button>
+
+            <a href="/load"><button class="fs-4 go-back-c btn"><i class="fal fa-chevron-left mr-1"></i><span style="margin-left: 10px">Go back</span></button></a>
+        </div>
+
+    <script>
+
+        let canvas = document.querySelector('#canvas');
+        let ctx = canvas.getContext('2d');
+
+        ctx.canvas.width = (window.innerWidth);
+        ctx.canvas.height = (window.innerHeight);
+        
+    </script>
+
+@endsection
