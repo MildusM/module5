09ec0d346ba276c4bedce6ca74974ce300ff066c
Ella Ahlstedt <ella.ahlstedt@learnet.se>
2021-12-03 10:56:47 +0100

    index done & load

--- module5_laravel/resources/views/pages/index.blade.php
+++ module5_laravel/resources/views/pages/index.blade.php
@@ -2,29 +2,29 @@
 
 @section('content')
 
-<section class="index">
+    <div class="index">
 
-    <div class="container text-center" style="width: 100%; ">
+        <div class="container text-center" style="width: 100%; ">
 
-        <div class="title">
-            <h1 style="font-size: 100px;">Genealogy</h1>
-        </div>
-        <div class="subheading">
-            <p>...create or load World lorem ipsum dolor imet sit lorem ipsum World lorem ipsum dolor imet sit lorem ipsum</p>
-        </div>
-        <div class="buttons row">
-            <div class="col-lg-6">
-                <a href="" class="btn btn-lg btn-gradient">Create</a>
+            <div class="title">
+                <h1 style="font-size: 100px;">Genealogy</h1>
             </div>
-            <div class="col-lg-6">
-                <a href="" class="btn btn-lg btn-outline-gradient"><span>Load</span></a>
+            <div class="subheading">
+                <p>Create or load world lorem ipsum dolor imet sit lorem ipsum World lorem ipsum dolor imet sit lorem ipsum</p>
             </div>
-        </div>
-        
+            <div class="buttons row">
+                <div class="col-lg-6">
+                    <a href="/create" class="btn btn-lg btn-gradient">Create</a>
+                </div>
+                <div class="col-lg-6">
+                    <a href="/load" class="btn btn-lg btn-outline-gradient"><span>Load</span></a>
+                </div>
+            </div>
+            
 
+
+        </div>
 
     </div>
-
-</section>
 
 @endsection
--- module5_laravel/resources/views/pages/load.blade.php
+++ module5_laravel/resources/views/pages/load.blade.php
@@ -2,4 +2,82 @@
 
 @section('content')
 
+    {{-- Page intro --}}
+    <div style="height: 350px;" class="gradient">
+        <a href="/index"><button class="fs-4 go-back"><i class="fal fa-chevron-left mr-1"></i><span style="margin-left: 10px">Go back</span></button></a>
+
+        <div class="page-intro">
+            <h1 style="font-size: 100px;">Load</h1>
+            <p style="font-size: large;">Here you can load the existent worlds that you can edit, delete or view.</p>
+        </div>
+
+    </div>
+
+    {{-- Worlds --}}
+
+    <div class="container load">
+        
+        <select name="orderBy" id="orderByWorld" class="input orderBy">
+            <option value="" disabled selected>Order By</option>
+            <option value="">Latest</option>
+            <option value="">A-Z</option>
+            <option value="">Z-A</option>
+        </select>
+
+        <div class="loadWorlds">
+
+            <div class="row listTitles">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-3">name</div>
+                <div class="col-lg-3">type</div>
+                <div class="col-lg-3">number of nodes</div>
+            </div>
+            
+        
+            <div class="row worldValues">
+
+                <div class="col-lg-3 radioInput">
+                    <button class="labelRadio"><i class="fas fa-check"></i></button>
+                </div>
+                <div class="col-lg-3">
+                    <p>nAMEoFtHEwORLD</p>
+                </div>
+                <div class="col-lg-3">
+                    <p>Branch</p>
+                </div>
+                <div class="col-lg-3">
+                    <p>15</p>
+                </div>
+
+            </div>
+
+            <div class="row worldValues">
+                
+                <div class="col-lg-3 radioInput">
+                    <button class="labelRadio"><i class="fas fa-check"></i></button>
+                </div>
+                <div class="col-lg-3">
+                    <p>nAMEoFtHEwORLD2</p>
+                </div>
+                <div class="col-lg-3">
+                    <p>Circular</p>
+                </div>
+                <div class="col-lg-3">
+                    <p>4</p>
+                </div>
+
+            </div>
+
+            <a href="/canvas"><button class="btn-gradient load-btn">Load</button></a>
+
+        </div>
+
+        
+
+        
+
+    </div>
+
+    
+
 @endsection
