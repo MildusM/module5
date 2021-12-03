0f702a670fa03daea43e131d9cff0f91de02dd04
Matilda Jönsson <matilda.jonsson4@learnet.se>
2021-12-03 10:56:42 +0100

    canvas

--- module5_laravel/resources/views/pages/create.blade.php
+++ module5_laravel/resources/views/pages/create.blade.php
@@ -4,7 +4,7 @@
 
     {{-- Page intro --}}
     <div style="height: 350px;" class="gradient">
-        <button class="fs-4 go-back"><i class="fal fa-chevron-left mr-1"></i><span style="margin-left: 10px">Go back</span></button>
+        <a href="/index"><button class="fs-4 go-back"><i class="fal fa-chevron-left mr-1"></i><span style="margin-left: 10px">Go back</span></button></a>
 
         <div class="page-intro">
             <h1 style="font-size: 100px;">Create</h1>
